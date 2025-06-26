7bf0403fe68d864a5f597399f2ee5f86e4f551a9
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-26 23:39:50 +0530

    Add files via upload

---
+++ UniverisityDriver.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class UniverisityDriver 
+{
+	public static void main(String[] args) {
+		Department d1 = new Department("hbcud", "cbd", 10, 20, 30, "bucdbd", "bcudcb", "bcided", "vchcd", 50, 50, "vhudhf", "ncvuh", "bcud", "bvcudhc", 50);
+		//d1.displayDepartment();
+		d1.displayCollege();
+	}
+	
+}
